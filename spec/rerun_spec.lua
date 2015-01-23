b5da8b6786e80706297ae3345abb382c38838c4a
Peter Dodds <peter.dodds@gmail.com>
2015-01-23 23:56:50 +0000

    Some more investigation into the arguments

--- spec/rerun_spec.lua
+++ spec/rerun_spec.lua
@@ -1,5 +1,5 @@
-local args = { n = select('#', ...), ... }
-print(...)
+-- Get the args from the first param
+local args = ...
 
 require("busted")
 
@@ -10,9 +10,11 @@ describe("sandpit suite", function()
     end)
 
     it("what is the args string?", function()
-      local s = ""
-      for i=1, args.n do
-        s = s .. ("%q"):format(tostring(args[i])) .. " "
+      local s = tostring(args)
+      if type(args) == "table" then
+        for k, v in pairs(args) do
+          s = ("%s[%s]=%s, "):format(s, tostring(k), tostring(v))
+        end
       end
       assert.falsy(s)
     end)
