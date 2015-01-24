1458569c92ff6bb4f6b18a66324f2a5467c2fa91
Peter Dodds <peter.dodds@gmail.com>
2015-01-24 01:35:21 +0000

    Throw an error when receiving a path for requires

--- lib/rerun.lua
+++ lib/rerun.lua
@@ -4,6 +4,10 @@ local rerun = {}
 
 local require = require
 function rerun.require(path)
+  if path:find("%.lua$") or path:find("[/\\:]") then
+    return error(("rerun.require must use dot syntax, %q is invalid"):format(path))
+  end
+
   return require(path)
 end
 
