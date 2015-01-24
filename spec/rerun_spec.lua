a6f665447d6552aabda74a9c5c621d4c995338f0
Peter Dodds <peter.dodds@gmail.com>
2015-01-24 00:14:38 +0000

    Test if we have a rerun lib

--- spec/rerun_spec.lua
+++ spec/rerun_spec.lua
@@ -1,22 +1,13 @@
--- Get the args from the first param
-local args = ...
+package.path = "./lib/?.lua;" .. package.path
 
-require("busted")
+local rerun = require 'rerun'
 
-describe("sandpit suite", function()
-  describe("first test", function()
-    it("is ok?", function()
-      assert.truthy("Yup.")
-    end)
+require 'busted'
 
-    it("what is the args string?", function()
-      local s = tostring(args)
-      if type(args) == "table" then
-        for k, v in pairs(args) do
-          s = ("%s[%s]=%s, "):format(s, tostring(k), tostring(v))
-        end
-      end
-      assert.falsy(s)
+describe("core suite", function()
+  describe("basics", function()
+    it("got rerun?", function()
+      assert.truthy(rerun)
     end)
   end)
 end)
