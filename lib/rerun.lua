966625674acf1582806d62300321370fbd5e959f
Peter Dodds <peter.dodds@gmail.com>
2015-01-24 11:42:41 +0000

    store lua require in rerun table

--- lib/rerun.lua
+++ lib/rerun.lua
@@ -2,13 +2,13 @@
 
 local rerun = {}
 
-local require = require
+rerun.__lua_require = require
 function rerun.require(path)
   if path:find("%.lua$") or path:find("[/\\:]") then
     return error(("rerun.require must use dot syntax, %q is invalid"):format(path))
   end
 
-  return require(path)
+  return rerun.__lua_require(path)
 end
 
 return rerun
