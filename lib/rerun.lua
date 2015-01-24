d18b27f8a7598c9526198bb78d96157f35d6dc18
Peter Dodds <peter.dodds@gmail.com>
2015-01-24 19:06:33 +0000

    Save the dependency of files required

--- lib/rerun.lua
+++ lib/rerun.lua
@@ -2,14 +2,25 @@
 
 local rerun = {}
 
+rerun.dependency = {}
+local function add_dependency(path, parent)
+  local d = rerun.dependency[path] or {}
+  d[parent] = true
+  rerun.dependency[path] = d
+end
+
 rerun.__lua_require = require
-function rerun.require(path)
+function rerun.require(path, parent)
   if path:find("%.lua$") or path:find("[/\\:]") then
     return error(("rerun.require must use dot syntax, %q is invalid"):format(path))
   end
 
+  if parent then
+    add_dependency(path, parent)
+  end
+
   local origEnv = getfenv(require)
-  local env = { require = rerun.require }
+  local env = { require = function(p) return rerun.require(p, path) end, }
   setmetatable(env, {
     __index = origEnv,
     __newindex = function(_,k,v) origEnv[k] = v end,
