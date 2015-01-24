8ce8bf9b34b9091663095e015617297058388334
Peter Dodds <peter.dodds@gmail.com>
2015-01-24 19:15:57 +0000

    Check dependency after requiring nested.lua

--- spec/rerun_spec.lua
+++ spec/rerun_spec.lua
@@ -20,6 +20,10 @@ describe("core suite", function()
 
     it("can require a file with inner requires", function()
       assert.truthy(rerun.require("testdata.nested"))
+
+      assert.are.equal(rerun.dependency, {
+          ["testdata.nested_sub1"] = "testdata.nested",
+        })
     end)
   end)
 end)
