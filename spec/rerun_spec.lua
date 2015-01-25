e3e7c2dfd95ff27c19fb5ff36ea0919b2a0bbb8a
Peter Dodds <peter.dodds@gmail.com>
2015-01-25 13:55:12 +0000

    Test rerun.reset

--- spec/rerun_spec.lua
+++ spec/rerun_spec.lua
@@ -11,6 +11,9 @@ describe("core suite", function()
     end)
 
     it("can require a simple file", function()
+      assert.are.same({}, rerun.stack)
+      assert.are.same({}, rerun.dependency)
+
       -- Dot syntax works
       assert.truthy(rerun.require("testdata.basic"))
 
@@ -18,18 +21,33 @@ describe("core suite", function()
           ["testdata.basic"] = {},
         }, rerun.dependency)
 
+      assert.are.same({}, rerun.stack)
+
       -- Paths are not allowed
       assert.error(function() return rerun.require("testdata/basic.lua") end)
     end)
 
+    it("can reset internals", function()
+      assert.are.same({
+          ["testdata.basic"] = {},
+        }, rerun.dependency)
+      assert.are.same({}, rerun.stack)
+
+      rerun.reset()
+
+      assert.are.same({}, rerun.stack)
+      assert.are.same({}, rerun.dependency)
+    end)
+
     it("can require a file with inner requires", function()
+      rerun.reset()
       assert.truthy(rerun.require("testdata.nested"))
 
       assert.are.same({
-          ["testdata.basic"] = {},
           ["testdata.nested"] = {},
           ["testdata.nested_sub1"] = { ["testdata.nested"] = true, },
         }, rerun.dependency)
+      assert.are.same({}, rerun.stack)
     end)
   end)
 end)
